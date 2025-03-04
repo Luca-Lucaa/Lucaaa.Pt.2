3f13d7e1277f270c14d235bc826e1876aaad0f33
Luca-Lucaa <luca.luca@tutamail.com>
2025-03-04 21:51:29 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,26 @@
+// config.js
+export const USER_CREDENTIALS = {
+  Admin: "Admino25!",
+  Scholli: "Scholli25",
+  Jamaica05: "Werwer55",
+};
+
+export const USER_EMOJIS = {
+  Admin: "👑",
+  Scholli: "🚀",
+  Jamaica05: "🎩",
+};
+
+export const THEME_CONFIG = {
+  palette: {
+    primary: { main: "#3b82f6" },
+    secondary: { main: "#dc004e" },
+    background: { default: "#e0e7ff", paper: "#ffffff" },
+    text: { primary: "#333" },
+  },
+};
+
+export const GUIDES = [
+  { name: "Anleitung PlockTV", path: "/guides/PlockTV.pdf" },
+  { name: "Anleitung 2", path: "/guides/guide2.pdf" },
+];
